6df2231ce19456291752928c7f01f16097611eb2
Grey Newell <greyshipscode@gmail.com>
2020-08-15 19:42:38 -0400

    + basic create record and/or table

--- models/collection.py
+++ models/collection.py
@@ -1,11 +1,46 @@
 import uuid
+import boto3
+
+dynamo = boto3.client("dynamodb")
 target = 'COLLECTION'
+tableName ='NajaraCollections'
+
+#check for table or create it
+try:
+    response = dynamo.describe_table(TableName=tableName)
+except dynamo.exceptions.ResourceNotFoundException:
+    response = dynamo.create_table(
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'id',
+                'AttributeType': 'S'
+            }
+        ],
+        TableName=tableName,
+        KeySchema=[
+            {
+                'AttributeName': 'id',
+                'KeyType': 'HASH'
+            }
+        ],
+        BillingMode='PAY_PER_REQUEST')
+    pass
 
 def create():
-    resultId = uuid.uuid4()
+    action = 'CREATE'
+    resultId = str(uuid.uuid4())
+
+    response = dynamo.put_item(
+            TableName=tableName,
+            Item={
+                'id': { 'S': resultId },
+                'name': { 'S': 'test name'},
+                'description': { 'S': 'test description'}
+                })
+    actionSuccess = response['ResponseMetadata']['HTTPStatusCode'] == 200
     return {
-            'action':'CREATE',
+            'action':action,
             'target':target,
-            'success':True,
-            'result-id':str(resultId)
+            'success':actionSuccess,
+            'result-id':resultId
             }
