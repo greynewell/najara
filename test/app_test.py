283aaa20e9f9bc35dcfbba940cd77bc4e2180e04
Grey Newell <greyshipscode@gmail.com>
2020-08-18 21:16:19 -0400

    + mocking dynamodb part 2

--- test/app_test.py
+++ test/app_test.py
@@ -1,13 +1,44 @@
-import json
-import pytest
-from app import app
-from moto import mock_dynamodb
+import json, pytest, os, boto3
+from moto import mock_dynamodb2
+tableName='NajaraCollections'
 
+
+@pytest.fixture(scope='function')
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
+    os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+    os.environ['AWS_SECURITY_TOKEN'] = 'testing'
+    os.environ['AWS_SESSION_TOKEN'] = 'testing'
+
+@pytest.fixture(scope='function')
+def dynamodb(aws_credentials):
+    with mock_dynamodb2():
+        yield boto3.client('dynamodb', region_name='us-east-1')
+
+@pytest.fixture(scope='function')
+def collection(dynamodb):
+    response = dynamodb.create_table(
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'id',
+                'AttributeType': 'S'
+            }
+        ],
+        TableName=tableName,
+        KeySchema=[
+            {
+                'AttributeName': 'id',
+                'KeyType': 'HASH'
+            }
+        ])
+    return response
 
 @pytest.fixture
-def gateway_factory():
+def gateway_factory(dynamodb, collection):
     from chalice.config import Config
     from chalice.local import LocalGateway
+    from app import app
 
     def create_gateway(config=None):
         if config is None:
@@ -17,8 +48,7 @@ def gateway_factory():
 
 
 class TestChalice(object):
-    @mock_dynamodb
-    def test_createCollection(self, gateway_factory):
+    def test_createCollection(self, gateway_factory, collection):
         gateway = gateway_factory()
         
         requestBody = {'name':'test', 'description':'testy'}
@@ -43,7 +73,6 @@ class TestChalice(object):
         assert testCollection['name'] == 'test'
         assert testCollection['description'] == 'testy'
 
-    @mock_dynamodb
     def test_createItem(self, gateway_factory):
         gateway = gateway_factory()
         response = gateway.handle_request(method='POST',
